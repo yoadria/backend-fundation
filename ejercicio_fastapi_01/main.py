ecc7cccdb39c7e38f25399c06336467d63ef97aa
Adria Hortoneda <tuadria@gmail.com>
2026-08-16 21:06:10 +0100

    fastapi_01 correccion 2

--- ejercicio_fastapi_01/main.py
+++ ejercicio_fastapi_01/main.py
@@ -1,6 +1,5 @@
 import uvicorn
-from fastapi import FastAPI, HTTPException
-import json
+from fastapi import FastAPI
 import logging
 
 logging.basicConfig(
@@ -12,21 +11,31 @@ logger = logging.getLogger(__name__)
 app = FastAPI()
 
 @app.get("/health")
-async def health():
+def health():
     return {"status":"ok"}
 
 @app.post("/v1/turn")
-async def turn(payload: dict):
-    try:
-        if not "user_id" in payload:
-            return {"status":400,"error": "falta user id"}
-        
-        if not "message" in payload  or payload.get("message").strip(" ") == "":
-            return {"status":400, "error": "falta message"}
-        return {"status":200}
-    except HTTPException as error:
-        logger.error(f"[ERROR] -> {error} ")
-        raise {"status": 401, "error": error}
+def turn(payload: dict):
+    if "user_id" not in payload:
+        return {"status":400,"error": "falta user id"}
+    
+    if "message" not in payload  or payload.get("message").strip(" ") == "":
+        return {"status":400, "error": "falta message"}
+    
+    response = {
+        "received": True,
+        "user": {
+            "id": payload.get("user_id"),
+            "name": payload.get("user_name")
+        },
+        "message": {
+            "text": payload.get("message"),
+            "length": len(payload.get("message"))
+        },
+        "companies_count": len(payload.get("allowed_company_ids"))
+    }
+
+    return response
 
 if __name__ == "__main__":
     uvicorn.run(
